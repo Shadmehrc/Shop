de1beddc0b1ae73708b6f4640c706dcfa6286f5f
a.aleagha <a.aleagha@UI-M-AMELI>
2022-07-25 17:28:39 +0430

    .

--- Shop-Products/Program.cs
+++ Shop-Products/Program.cs
@@ -1,9 +1,10 @@
+using System;
+using System.Net;
+using System.Security.Authentication;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
 namespace Shop_Products
 {
@@ -18,34 +19,27 @@ namespace Shop_Products
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
-            //Host.CreateDefaultBuilder(args)
-            //    .ConfigureWebHostDefaults(webBuilder =>
-            //    {
-            //        webBuilder
-            //            .UseKestrel()
-            //            .UseIIS()
-            //            .UseUrls($"http://*:{_applicationPort}")
-            //            .UseStartup<Startup>();
-            //    });
             Host.CreateDefaultBuilder(args)
-                .ConfigureLogging(logging =>
-                {
-                    logging.ClearProviders();
-
-                })
-                .ConfigureServices((context, services) =>
-                {
-                    services.Configure<KestrelServerOptions>(
-                        context.Configuration.GetSection("Kestrel"));
-                })
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>().UseUrls($"http://*:{_applicationPort}");
+                    webBuilder
+                        .UseKestrel()
+                        .UseIIS()
+                        .UseStartup<Startup>()
+                        .ConfigureKestrel((context, options) =>
+                        {
+                            options.Listen(IPAddress.Any, Convert.ToInt32(_applicationPort), listenOptions =>
+                            {
+                                listenOptions.UseHttps(o => o.SslProtocols = SslProtocols.Tls12);
+                                listenOptions.UseConnectionLogging();
+                                listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+                            });
+                        });
                 });
 
         private static void InitialConfig(string[] args)
         {
-            var env = new WebHostBuilder(); 
+            var env = new WebHostBuilder();
             var environmentName = env.GetSetting("environment");
 
             _config = new ConfigurationBuilder().AddCommandLine(args)
